dd0bf67c947b5e50d6b25167d5140a621b9e48c9
mygithubshivam <kumarshivam3127@gmail.com>
2024-06-12 12:23:13 +0530

    first commit

---
+++ routes/authRoute.js
@@ -0,0 +1,31 @@
+import express from "express";
+import {
+  registerController,
+  loginController,
+  testController,
+  forgotPasswordController,
+} from "../controllers/authController.js";
+import { requireSignIn, isAdmin } from "../middlewares/authMiddleware.js";
+
+//router object
+const router = express.Router();
+
+//Register
+router.post("/register", registerController);
+//Login
+router.post("/login", loginController);
+//forgot-password
+router.post("/forgot-password", forgotPasswordController);
+//test
+router.get("/test", requireSignIn, isAdmin, testController);
+
+// protected User-route auth
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+// protected Admin-route auth
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
+export default router;
---
+++ server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoute from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import cors from "cors";
+
+// Configure environment variables
+dotenv.config();
+
+// Database configuration
+connectDB();
+
+// Create Express app and middlewares
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Set up morgan middleware to log HTTP requests
+app.use(morgan("dev")); // or 'dev', 'tiny', etc. based on your preference
+
+//routes
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/category", categoryRoutes);
+
+// REST API
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`.bgCyan.white);
+});
