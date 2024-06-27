cbc1753461df788ecf8b7ee91b60d5fc2af3cc9f
mygithubshivam <kumarshivam3127@gmail.com>
2024-06-27 23:56:04 +0530

    added changes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,6 +6,7 @@ import {
   deleteProductController,
   getProductController,
   getSingleProductController,
+  productFiltersController,
   productPhotoController,
   updateProductController,
 } from "../controllers/productController.js";
@@ -41,4 +42,14 @@ router.get("/product-photo/:pid", productPhotoController);
 
 //delete product
 router.delete("/delete-product/:pid", deleteProductController);
+
+//filter product
+router.post("/product-filters", productFiltersController);
+
+//product count
+router.get("/product-count", productCountController);
+
+//product per page
+router.get("/product-list/:page", productListController);
+
 export default router;
