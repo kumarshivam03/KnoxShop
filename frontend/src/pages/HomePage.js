dbf60c65e2dd2b22c8fd007f2d7fbed14ceff414
mygithubshivam <kumarshivam3127@gmail.com>
2024-08-28 09:45:00 +0530

    final files added

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -110,7 +110,7 @@ const HomePage = () => {
     <Layout title={"ALl Products - Best offers "}>
       {/* banner image */}
       <img
-        src="/images/banner.png"
+        src="/images/banner1.png"
         className="banner-img"
         alt="bannerimage"
         width={"100%"}
